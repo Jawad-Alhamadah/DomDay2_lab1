8e534d52754cd8cc3c82f67816edf5a63e3f0976
vortix2950 <vortix2950@ksu.edu>
2024-09-12 10:59:52 +0300

    added text under the buttons

--- script.js
+++ script.js
@@ -1,64 +1,12 @@
 
-let button_1 = document.getElementById("button1")
-let button_2 = document.getElementById("button2")
-let button_3 = document.getElementById("button3")
+let elements = [ {element:document.getElementById("button1"), text:"button1" },
+                 {element:document.getElementById("button2"), text:"button2"},
+                 {element:document.getElementById("button3"), text:"button3" }
+]
 
-
-
-
-let hidden = document.createElement("span")
-// let active = document.createElement("span")
-
-hidden = document.createElement("span")
-hidden.classList.add("hidden")
-hidden.innerText="Button1"
-button_1.appendChild(hidden)
-
-hidden = document.createElement("span")
-hidden.classList.add("hidden")
-hidden.innerText="Button2"
-button_2.appendChild(hidden)
-
-hidden = document.createElement("span")
-hidden.classList.add("hidden")
-hidden.innerText="Button3"
-button_3.appendChild(hidden)
-// active.innerText=""
-
-// button_1.appendChild(active)
-
-
-
-// hidden = document.createElement("span")
-// active = document.createElement("span")
-
-// hidden.classList.add("hidden")
-// active.classList.add("active")
-
-// hidden.innerText="Button2"
-
-// active.innerText=""
-
-// button_2.appendChild(hidden)
-// button_2.appendChild(active)
-
-
-// hidden = document.createElement("span")
-// active = document.createElement("span")
-
-// hidden.classList.add("hidden")
-// active.classList.add("active")
-
-// hidden.innerText="Button3"
-
-// active.innerText=""
-
-// button_3.appendChild(hidden)
-// button_3.appendChild(active)
-
-
-
-
-
-
-
+elements.map(e => {
+    let hidden = document.createElement("span")
+    hidden.classList.add("hidden")
+    hidden.innerText=e.text
+    e.element.appendChild(hidden)
+})
